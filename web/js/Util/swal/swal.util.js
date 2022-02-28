4c778253626fa699a6c98c97ba0943b7a2cd7bce
rinki20 <rinkirana200901@gmail.com>
2022-02-28 10:45:07 +0530

    login module swal functionality added

--- web/js/Util/swal/swal.util.js
+++ web/js/Util/swal/swal.util.js
@@ -1,10 +1,54 @@
 // possible icons : question , error, warning, success
 
 function showSwal(title, text, icon = "success", footer = "") {
-  Swal.fire({
-    icon,
-    title,
-    text,
+    Swal.fire({
+        icon,
+        title,
+        text,
     footer,
-  });
+    });
 }
+
+function showSwalCallback(title, timer, callback, icon = "success")
+{
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+        }
+    });
+
+    Toast.fire({
+        icon,
+        title
+    });
+
+    callback();
+}
+
+function showSwalTimer(title, timer, icon = "success")
+{
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+        }
+    });
+
+    Toast.fire({
+        icon,
+        title
+    });
+
+}
+
