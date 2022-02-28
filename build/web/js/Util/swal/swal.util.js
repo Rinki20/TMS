1244371be7738d6ee4543dde295b947eb0a3f898
Rinki20 <90687554+Rinki20@users.noreply.github.com>
2022-02-28 10:56:10 +0530

    Update swal.util.js

--- build/web/js/Util/swal/swal.util.js
+++ build/web/js/Util/swal/swal.util.js
@@ -9,45 +9,18 @@ function showSwal(title, text, icon = "success", footer = "") {
     });
 }
 
-function showSwalCallback(title, timer, callback, icon = "success")
-{
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
-        }
-    });
-
-    Toast.fire({
+function showConfirmSwal(title,text,confirmButtonText,callBackfunction,icon="warning",showCancelButton=true) {
+    Swal.fire({
+        title,
+        text,
         icon,
-        title
-    });
-
-    callback();
-}
-
-function showSwalTimer(title, timer, icon = "success")
-{
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer);
-            toast.addEventListener('mouseleave', Swal.resumeTimer);
+        showCancelButton,
+        confirmButtonText,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            callBackfunction();
         }
-    });
-
-    Toast.fire({
-        icon,
-        title
-    });
-
+    })
 }
